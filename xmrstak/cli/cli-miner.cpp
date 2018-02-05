e303943b1202b41968107435dbcd4c1a2d99e678
Alexey Galkin <alexey.galkin@kaspersky.com>
2018-02-05 19:03:44 +0300

    Add export for main function

--- xmrstak/cli/cli-miner.cpp
+++ xmrstak/cli/cli-miner.cpp
@@ -1,26 +1,3 @@
- /*
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  *
-  * Additional permission under GNU GPL version 3 section 7
-  *
-  * If you modify this Program, or any covered work, by linking or combining
-  * it with OpenSSL (or a modified version of that library), containing parts
-  * covered by the terms of OpenSSL License and SSLeay License, the licensors
-  * of this Program grant you additional permission to convey the resulting work.
-  *
-  */
-
 #include "xmrstak/misc/executor.hpp"
 #include "xmrstak/backend/miner_work.hpp"
 #include "xmrstak/backend/globalStates.hpp"
@@ -309,6 +286,18 @@ void do_guided_config()
 	configTpl.write(params::inst().configFile);
 	std::cout<<"Configuration stored in file '"<<params::inst().configFile<<"'"<<std::endl;
 }
+
+extern "C"  {
+#ifdef WIN32
+__declspec(dllexport) 
+#endif
+
+int entry_point(int argc, char *argv[])
+{
+	environment::inst(&env);
+	return main(argc, *argv[]);
+}
+} // extern "C"
 
 int main(int argc, char *argv[])
 {
